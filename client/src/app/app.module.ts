e4a2699a3e10d955f49a8171fb4027595bec234f
alexryanjones <alexryanjones@gmx.com>
2023-04-08 16:38:33 +0200

    add: component structure

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { GameWindowComponent } from './game-window/game-window.component';
+import { GamePosterComponent } from './game-poster/game-poster.component';
+import { GameDetailsComponent } from './game-details/game-details.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    GameWindowComponent,
+    GamePosterComponent,
+    GameDetailsComponent,
+    SearchBarComponent,
   ],
   imports: [
     BrowserModule
---
+++ client/src/app/game-poster/game-poster.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-poster',
+  templateUrl: './game-poster.component.html',
+  styleUrls: ['./game-poster.component.css']
+})
+export class GamePosterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
